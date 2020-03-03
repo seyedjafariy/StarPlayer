b9e15fd90f3febeeb4d89ca1543603e26fd2d770
hosseiniSeyRo <zoho1381@gmail.com>
2020-03-03 09:39:56 +0330

    design playerFragment view

---
+++ app/src/main/java/com/worldsnas/starplayer/view/PlayerFragment.kt
@@ -0,0 +1,24 @@
+package com.worldsnas.starplayer.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.worldsnas.starplayer.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PlayerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_player, container, false)
+    }
+
+}
