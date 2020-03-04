7a7386626b8e490ff6c9a402866cffff43a651b5
Ali Raghebi <aliraghebiii@gmail.com>
2020-03-04 17:04:13 +0330

    changed onbind to pass object instead of position

--- app/src/main/java/com/worldsnas/starplayer/core/BaseViewHolder.kt
+++ app/src/main/java/com/worldsnas/starplayer/core/BaseViewHolder.kt
@@ -3,6 +3,6 @@ package com.worldsnas.starplayer.core
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
-abstract class BaseViewHolder(itemView: View) : ViewHolder(itemView) {
-    abstract fun onBind(position: Int)
+abstract class BaseViewHolder<T>(itemView: View) : ViewHolder(itemView) {
+    abstract fun onBind(obj: T)
 }
