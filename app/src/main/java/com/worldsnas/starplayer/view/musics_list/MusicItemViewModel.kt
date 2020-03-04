bb11c9cb787174271c21702faff0c1f759c4162e
Ali Raghebi <aliraghebiii@gmail.com>
2020-03-04 15:33:06 +0330

    use livedata instead

--- app/src/main/java/com/worldsnas/starplayer/view/musics_list/MusicItemViewModel.kt
+++ app/src/main/java/com/worldsnas/starplayer/view/musics_list/MusicItemViewModel.kt
@@ -1,12 +1,17 @@
 package com.worldsnas.starplayer.view.musics_list
 
-import androidx.databinding.ObservableField
+import androidx.lifecycle.MutableLiveData
 import com.worldsnas.starplayer.model.Music
 
 
-class MusicItemViewModel(private val music: Music,private val listener:Listener) {
-    var musicTitle: ObservableField<String> = ObservableField(music.title)
-    var musicArtist: ObservableField<String> = ObservableField(music.artist)
+class MusicItemViewModel(private val music: Music, private val listener: Listener) {
+    var musicTitle: MutableLiveData<String> = MutableLiveData()
+    var musicArtist: MutableLiveData<String> = MutableLiveData()
+
+    init {
+        musicTitle.value = music.title
+        musicArtist.value = music.artist
+    }
 
     interface Listener {
         fun onItemClick()
