b8fec72af49a1a2088d35a98bdf650cda759cd78
Ali Raghebi <aliraghebiii@gmail.com>
2020-03-04 17:06:01 +0330

    remove oncreate from fragment class

--- app/src/main/java/com/worldsnas/starplayer/view/musics_list/MusicsListFragment.kt
+++ app/src/main/java/com/worldsnas/starplayer/view/musics_list/MusicsListFragment.kt
@@ -15,11 +15,6 @@ class MusicsListFragment : Fragment() {
     private var _binding: FragmentMusicsListBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
