90efa3a7bad2fa5db9f6bcc7e07896d04f8af43a
MohammadR Haddad <neptu39701@gmail.com>
2020-03-10 17:16:26 +0330

    refactor player fragment to component builder and bind instance

--- app/src/main/java/com/worldsnas/starplayer/di/PlayerFragmentComponent.kt
+++ app/src/main/java/com/worldsnas/starplayer/di/PlayerFragmentComponent.kt
@@ -1,10 +1,22 @@
 package com.worldsnas.starplayer.di
 
 import com.worldsnas.starplayer.view.PlayerFragment
+import dagger.BindsInstance
 import dagger.Component
 
-@Component (modules = [Androidmodule::class])
+@Component(dependencies = [AppComponent::class])
 interface PlayerFragmentComponent {
 
-    fun inject (playerFragment: PlayerFragment)
+    fun inject(playerFragment: PlayerFragment)
+
+    @Component.Builder
+    interface Builder {
+
+        fun build(): PlayerFragmentComponent
+
+        fun mainApplicationComponent(appComponent: AppComponent): Builder
+
+        @BindsInstance
+        fun playerFragment(playerFragment: PlayerFragment): Builder
+    }
 }
