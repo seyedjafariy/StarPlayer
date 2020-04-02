6f0e1092a4df21b784d80fe023a8748917d76f12
Mohammad HF <mohammadfakhraee007@gmail.com>
2020-04-03 01:50:46 +0430

    Exo set up

--- app/src/main/java/com/worldsnas/starplayer/MainActivity.kt
+++ app/src/main/java/com/worldsnas/starplayer/MainActivity.kt
@@ -14,8 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
     }
 }
