722040b9015b3a8512f835233856a17b1b9f13dc
neptu39701 <neptu39701@gmail.com>
2020-04-03 21:26:02 +0430

    make uri String  in model

--- app/src/main/java/com/worldsnas/starplayer/model/Music.kt
+++ app/src/main/java/com/worldsnas/starplayer/model/Music.kt
@@ -1,11 +1,9 @@
 package com.worldsnas.starplayer.model
 
-import android.net.Uri
-
 data class Music(
     val id: Int,
     val title: String,
     val artist: String,
     val album: String,
-    val uri: Uri
+    val uri: String
 )
