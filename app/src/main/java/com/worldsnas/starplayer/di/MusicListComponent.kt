19b2ab134f62fafbb8be77bdd0e88be8b27ceed3
neptu39701 <neptu39701@gmail.com>
2020-04-12 00:08:24 +0430

    Add context provider to component

--- app/src/main/java/com/worldsnas/starplayer/di/MusicListComponent.kt
+++ app/src/main/java/com/worldsnas/starplayer/di/MusicListComponent.kt
@@ -1,10 +1,14 @@
 package com.worldsnas.starplayer.di
 
+import android.app.Application
+import android.content.Context
 import com.worldsnas.starplayer.view.musics_list.MusicsListFragment
+import dagger.BindsInstance
 import dagger.Component
 
+
 @FragmentScope
-@Component(dependencies = [AppComponent::class], modules = [ViewModelModule::class])
+@Component(dependencies = [AppComponent::class], modules = [ViewModelModule::class , LocalMusicModule::class])
 interface MusicListComponent {
 
     fun inject(musicsListFragment: MusicsListFragment)
@@ -14,5 +18,7 @@ interface MusicListComponent {
         fun build(): MusicListComponent
 
         fun appComponent(appComponent: AppComponent): Builder
+        @BindsInstance
+        fun contextProvider(context: Context): Builder
     }
 }
