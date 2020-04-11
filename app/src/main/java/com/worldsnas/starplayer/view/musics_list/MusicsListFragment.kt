f93e958ea03024f6f471f9b880db8ccc69559839
neptu39701 <neptu39701@gmail.com>
2020-04-12 01:25:55 +0430

    Add ViewModel to Fragment

--- app/src/main/java/com/worldsnas/starplayer/view/musics_list/MusicsListFragment.kt
+++ app/src/main/java/com/worldsnas/starplayer/view/musics_list/MusicsListFragment.kt
@@ -1,20 +1,27 @@
 package com.worldsnas.starplayer.view.musics_list
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.worldsnas.starplayer.App
-
-import com.worldsnas.starplayer.R
 import com.worldsnas.starplayer.databinding.FragmentMusicsListBinding
 import com.worldsnas.starplayer.di.DaggerMusicListComponent
 import com.worldsnas.starplayer.di.MusicListComponent
 import com.worldsnas.starplayer.model.Music
+import com.worldsnas.starplayer.view.ViewModelFactory
+import javax.inject.Inject
 
 class MusicsListFragment : Fragment() {
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+    private val musicListViewModel by lazy {
+        viewModelFactory.create(MusicListViewModel::class.java)
+    }
     private var _binding: FragmentMusicsListBinding? = null
     private val binding get() = _binding!!
     lateinit var musicListComponent: MusicListComponent
@@ -32,14 +39,11 @@ class MusicsListFragment : Fragment() {
 
         daggerSetup()
 
-        val musics = ArrayList<Music>()
-        musics.add(Music(0, "Test", "Ali", "Single", "Pop","somewhere"))
-        musics.add(Music(0, "Test", "Ali", "Single", "Pop","somewhere"))
-        musics.add(Music(0, "Test", "Ali", "Single", "Pop","somewhere"))
-        musics.add(Music(0, "Test", "Ali", "Single", "Pop","somewhere"))
-        musics.add(Music(0, "Test", "Ali", "Single", "Pop","somewhere"))
-        musics.add(Music(0, "Test", "Ali", "Single", "Pop","somewhere"))
-        binding.recyclerview.adapter = MusicsListAdapter(musics)
+        val musicObserver = Observer<List<Music>> {
+
+            binding.recyclerview.adapter = MusicsListAdapter(it as ArrayList<Music>)
+        }
+        musicListViewModel.getAllMusic().observe(viewLifecycleOwner, musicObserver)
     }
 
     override fun onDestroyView() {
@@ -50,7 +54,8 @@ class MusicsListFragment : Fragment() {
     private fun daggerSetup() {
 
         musicListComponent = DaggerMusicListComponent.builder()
-            .appComponent((activity!!.application as App).appComponent).build()
+            .appComponent((activity!!.application as App).appComponent).contextProvider(context!!)
+            .build()
         musicListComponent.inject(this)
     }
 }
