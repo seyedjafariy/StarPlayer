ff442329bea3d5f09a4ffbc117e0b7c3da949452
neptu39701 <neptu39701@gmail.com>
2020-04-12 00:09:01 +0430

    Optimized Imports

--- app/src/main/java/com/worldsnas/starplayer/view/ViewModelFactory.kt
+++ app/src/main/java/com/worldsnas/starplayer/view/ViewModelFactory.kt
@@ -2,18 +2,13 @@ package com.worldsnas.starplayer.view
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.worldsnas.starplayer.model.LocalMusicProviderImpl
-import com.worldsnas.starplayer.view.musics_list.MusicListViewModel
-import java.lang.IllegalArgumentException
 import javax.inject.Inject
 import javax.inject.Provider
-import javax.inject.Singleton
 
 
-@Singleton
 class ViewModelFactory @Inject constructor(private val viewModels: MutableMap<Class<out ViewModel>, Provider<ViewModel>>) :
-    ViewModelProvider.Factory {
+	ViewModelProvider.Factory {
 
-    override fun <T : ViewModel> create(modelClass: Class<T>): T =
-        viewModels[modelClass]?.get() as T
+	override fun <T : ViewModel> create(modelClass: Class<T>): T =
+		viewModels[modelClass]?.get() as T
 }
