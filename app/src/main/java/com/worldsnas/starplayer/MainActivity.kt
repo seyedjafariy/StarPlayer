c88aa174632d54907d4f6de2cdcab118d00b0560
neptu39701 <neptu39701@gmail.com>
2020-04-13 01:35:04 +0430

    Permission check moved to fragment & fragment setup

--- app/src/main/java/com/worldsnas/starplayer/MainActivity.kt
+++ app/src/main/java/com/worldsnas/starplayer/MainActivity.kt
@@ -18,77 +18,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 100
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
-        checkPermission()
-    }
-
-    private fun checkPermission() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            )
-            != PackageManager.PERMISSION_GRANTED
-        ) {
-
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE
-                )
-            ) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
-                )
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        } else {
-
-            // Permission has already been granted
-            Toast.makeText(this, "Already Granted", Toast.LENGTH_SHORT).show()
-        }
-
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>, grantResults: IntArray
-    ) {
-        when (requestCode) {
-            MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE -> {
-                // If request is cancelled, the result arrays are empty.
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Toast.makeText(this, "Storage Permission Needed", Toast.LENGTH_SHORT).show()
-                }
-                return
-            }
-            // Add other 'when' lines to check for other
-            // permissions this app might request.
-            else -> {
-                // Ignore all other requests.
-                Toast.makeText(this, "Did Not Granted", Toast.LENGTH_SHORT).show()
-            }
-        }
     }
 }
 
--- app/src/main/java/com/worldsnas/starplayer/view/musics_list/MusicsListFragment.kt
+++ app/src/main/java/com/worldsnas/starplayer/view/musics_list/MusicsListFragment.kt
@@ -1,11 +1,17 @@
 package com.worldsnas.starplayer.view.musics_list
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.worldsnas.starplayer.App
 import com.worldsnas.starplayer.databinding.FragmentMusicsListBinding
@@ -15,16 +21,25 @@ import com.worldsnas.starplayer.model.Music
 import com.worldsnas.starplayer.view.ViewModelFactory
 import javax.inject.Inject
 
+
 class MusicsListFragment : Fragment() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
-    private val musicListViewModel by lazy {
-        viewModelFactory.create(MusicListViewModel::class.java)
+
+    private val musicListViewModel by
+    viewModels<MusicListViewModel> { viewModelFactory }
+
+    private val READ_EXTERNAL_STORAGE_REQUEST_CODE = 100
+
+    private val musicListAdapter by lazy {
+        MusicsListAdapter(musics)
     }
+
     private var _binding: FragmentMusicsListBinding? = null
     private val binding get() = _binding!!
     lateinit var musicListComponent: MusicListComponent
+    var musics = ArrayList<Music>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,17 +48,14 @@ class MusicsListFragment : Fragment() {
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         daggerSetup()
+        storagePermissionCheck()
 
-        val musicObserver = Observer<List<Music>> {
+        binding.recyclerview.adapter = musicListAdapter
 
-            binding.recyclerview.adapter = MusicsListAdapter(it as ArrayList<Music>)
-        }
-        musicListViewModel.getAllMusic().observe(viewLifecycleOwner, musicObserver)
     }
 
     override fun onDestroyView() {
@@ -51,11 +63,74 @@ class MusicsListFragment : Fragment() {
         _binding = null
     }
 
+    private fun liveDataSetup() {
+
+        val musicObserver = Observer<List<Music>> {
+            musics = it as ArrayList<Music>
+            Log.d("tag", musics.toString())
+            musicListAdapter.submitList(musics)
+        }
+        musicListViewModel.postMusic().observe(viewLifecycleOwner, musicObserver)
+
+
+        Log.d("tag", musics.toString())
+
+    }
+
     private fun daggerSetup() {
 
         musicListComponent = DaggerMusicListComponent.builder()
-            .appComponent((activity!!.application as App).appComponent).contextProvider(context!!)
+            .appComponent((activity!!.application as App).appComponent)
             .build()
         musicListComponent.inject(this)
     }
+
+    private fun storagePermissionCheck() {
+        if (checkSelfPermission()) {
+            Toast.makeText(context, "Already Granted", Toast.LENGTH_SHORT).show()
+            liveDataSetup()
+        } else
+            requestPermission()
+    }
+
+    private fun checkSelfPermission(): Boolean {
+        val result = ContextCompat.checkSelfPermission(
+            context!!,
+            READ_EXTERNAL_STORAGE
+        )
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            activity!!,
+            arrayOf(READ_EXTERNAL_STORAGE),
+            READ_EXTERNAL_STORAGE_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            READ_EXTERNAL_STORAGE_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    liveDataSetup()
+
+                } else {
+                    // disable function did not granted
+                    Toast.makeText(context, "Need Permission to Access Songs", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+            else -> return
+        }
+
+    }
+
 }
+
