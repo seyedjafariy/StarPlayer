fe9c0bbc1af032e2a216cafc97bda4cd4316b86a
neptu39701 <neptu39701@gmail.com>
2020-04-12 22:52:00 +0430

    Fix inject to ViewModel & separate Livedata from Coroutines

--- app/src/main/java/com/worldsnas/starplayer/view/musics_list/MusicListViewModel.kt
+++ app/src/main/java/com/worldsnas/starplayer/view/musics_list/MusicListViewModel.kt
@@ -1,33 +1,39 @@
 package com.worldsnas.starplayer.view.musics_list
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.worldsnas.starplayer.model.LocalMusicProvider
 import com.worldsnas.starplayer.model.LocalMusicProviderImpl
 import com.worldsnas.starplayer.model.Music
 import kotlinx.coroutines.*
 import javax.inject.Inject
 
-class MusicListViewModel @Inject constructor(private val localMusicProviderImpl: LocalMusicProviderImpl) :
+class MusicListViewModel @Inject constructor(private val localMusicProvider: LocalMusicProvider) :
     ViewModel() {
 
+
     private val postMusicList = MutableLiveData<List<Music>>()
-    private val viewModelJob = SupervisorJob()
 
-    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
-    fun getAllMusic(): MutableLiveData<List<Music>> {
-        uiScope.launch {
-            val musicsList = localMusicProviderImpl.getAllMusic()
+    private fun getAllMusic() {
+        viewModelScope.launch {
+            val musicsList = localMusicProvider.getAllMusic()
 
             postMusicList.postValue(musicsList)
         }
+    }
+
+    fun postMusic(): LiveData<List<Music>> {
+        getAllMusic()
         return postMusicList
     }
 
     override fun onCleared() {
         super.onCleared()
-        viewModelJob.cancel()
+        viewModelScope.cancel()
     }
 
 }
