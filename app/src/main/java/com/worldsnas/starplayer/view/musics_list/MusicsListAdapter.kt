49334ae471458219cca27a0cd54d1a930c35ac62
neptu39701 <neptu39701@gmail.com>
2020-04-13 21:09:26 +0430

    Add ListAdapter and DiffHandler

--- app/src/main/java/com/worldsnas/starplayer/view/musics_list/MusicsListAdapter.kt
+++ app/src/main/java/com/worldsnas/starplayer/view/musics_list/MusicsListAdapter.kt
@@ -3,14 +3,18 @@ package com.worldsnas.starplayer.view.musics_list
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncDifferConfig
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.worldsnas.starplayer.core.BaseViewHolder
 import com.worldsnas.starplayer.databinding.ItemMusicBinding
 import com.worldsnas.starplayer.model.Music
 
-class MusicsListAdapter(private val musics: ArrayList<Music>) :
-    RecyclerView.Adapter<BaseViewHolder<Music>>() {
+class MusicsListAdapter() :
+    ListAdapter<Music, MusicListItemViewHolder>(DiffCallback) {
 
+    private val musics = ArrayList<Music>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicListItemViewHolder {
         val musicViewBinding: ItemMusicBinding =
             ItemMusicBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -21,13 +25,8 @@ class MusicsListAdapter(private val musics: ArrayList<Music>) :
         return musics.size
     }
 
-    override fun onBindViewHolder(holder: BaseViewHolder<Music>, position: Int) {
+    override fun onBindViewHolder(holder: MusicListItemViewHolder, position: Int) {
         holder.onBind(musics[position])
-    }
-
-    fun submitList(musicList: List<Music>) {
-        musics += musicList
-        notifyDataSetChanged()
     }
 }
 
@@ -37,5 +36,15 @@ class MusicListItemViewHolder(private val mBinding: ItemMusicBinding) :
     override fun onBind(obj: Music) {
         mBinding.txtMusicTitle.text = obj.title
         mBinding.txtArtist.text = obj.artist
+    }
+}
+
+object DiffCallback : DiffUtil.ItemCallback<Music>() {
+    override fun areItemsTheSame(oldItem: Music, newItem: Music): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Music, newItem: Music): Boolean {
+        return oldItem.id == newItem.id
     }
 }
