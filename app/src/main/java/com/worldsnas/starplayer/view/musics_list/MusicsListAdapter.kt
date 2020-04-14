ca3db7b78f035692cd395bb1127bc0a147a89efd
neptu39701 <neptu39701@gmail.com>
2020-04-15 01:02:12 +0430

    Send Data with bundle

--- app/src/main/java/com/worldsnas/starplayer/view/musics_list/MusicsListAdapter.kt
+++ app/src/main/java/com/worldsnas/starplayer/view/musics_list/MusicsListAdapter.kt
@@ -1,10 +1,12 @@
 package com.worldsnas.starplayer.view.musics_list
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import com.worldsnas.starplayer.R
 import com.worldsnas.starplayer.core.BaseViewHolder
 import com.worldsnas.starplayer.databinding.ItemMusicBinding
 import com.worldsnas.starplayer.model.Music
@@ -30,7 +32,25 @@ class MusicListItemViewHolder(private val mBinding: ItemMusicBinding) :
     override fun onBind(obj: Music) {
         mBinding.txtMusicTitle.text = obj.title
         mBinding.txtArtist.text = obj.artist
+
+        sendToPlay(obj)
     }
+
+    private fun sendToPlay(obj: Music) {
+        mBinding.root.setOnClickListener {
+            val bundle = bundleOf(
+                "id" to obj.id,
+                "title" to obj.title,
+                "artist" to obj.artist,
+                "album" to obj.album,
+                "genre" to obj.genre,
+                "address" to obj.address
+            )
+            itemView.findNavController()
+                .navigate(R.id.action_musicsListFragment_to_playerFragment, bundle)
+        }
+    }
+
 }
 
 object DiffCallback : DiffUtil.ItemCallback<Music>() {
