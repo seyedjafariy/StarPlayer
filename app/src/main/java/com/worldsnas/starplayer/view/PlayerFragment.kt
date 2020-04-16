b5d8190e4d848fed52322c7d9244ab0838df6a4f
neptu39701 <neptu39701@gmail.com>
2020-04-16 21:38:19 +0430

    Get Parcel data

--- app/src/main/java/com/worldsnas/starplayer/view/PlayerFragment.kt
+++ app/src/main/java/com/worldsnas/starplayer/view/PlayerFragment.kt
@@ -2,27 +2,36 @@ package com.worldsnas.starplayer.view
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.worldsnas.starplayer.App
+import com.worldsnas.starplayer.R
 import com.worldsnas.starplayer.databinding.FragmentPlayerBinding
 import com.worldsnas.starplayer.di.DaggerPlayerComponent
 import com.worldsnas.starplayer.di.PlayerComponent
+import com.worldsnas.starplayer.model.Music
+import com.worldsnas.starplayer.view.musics_list.ProvideMusicListener
 
 /**
  * A simple [Fragment] subclass.
  */
 class PlayerFragment : Fragment() {
-
+    private val musicInfoBundleKey = "musicInfo"
+    private val preAddress = "content://media/"
     private lateinit var playerComponent: PlayerComponent
     private var exoPlayer: SimpleExoPlayer? = null
     private var viewBinding: FragmentPlayerBinding? = null
+    private val music by lazy {
+        arguments?.getParcelable<Music>(musicInfoBundleKey)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,7 +53,8 @@ class PlayerFragment : Fragment() {
         exoPlayer = SimpleExoPlayer.Builder(context!!).build()
         viewBinding?.exoControlView?.player = exoPlayer
 
-        val filePath = "content://media/" + arguments?.getString("address")
+
+        val filePath = preAddress + music?.address
         val mediaSource = buildMediaSource(Uri.parse(filePath))
         exoPlayer?.prepare(mediaSource, false, false)
     }
@@ -67,8 +77,8 @@ class PlayerFragment : Fragment() {
     }
 
     private fun setMusicArguments() {
-        viewBinding?.tvMusicTitle?.text = arguments?.getString("title")
-        viewBinding?.tvMusicArtist?.text = arguments?.getString("artist")
+        viewBinding?.tvMusicTitle?.text = music?.title
+        viewBinding?.tvMusicArtist?.text = music?.artist
 
     }
 
@@ -77,4 +87,6 @@ class PlayerFragment : Fragment() {
             .appComponent((activity!!.application as App).appComponent).build()
         playerComponent.inject(this)
     }
+
+
 }
