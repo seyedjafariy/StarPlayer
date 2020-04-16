be3d474777f58e3504c03aaaac636149944e571d
neptu39701 <neptu39701@gmail.com>
2020-04-16 21:36:23 +0430

    implement send Callback to Fragment

--- app/src/main/java/com/worldsnas/starplayer/view/musics_list/MusicsListAdapter.kt
+++ app/src/main/java/com/worldsnas/starplayer/view/musics_list/MusicsListAdapter.kt
@@ -2,22 +2,20 @@ package com.worldsnas.starplayer.view.musics_list
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import com.worldsnas.starplayer.R
 import com.worldsnas.starplayer.core.BaseViewHolder
 import com.worldsnas.starplayer.databinding.ItemMusicBinding
 import com.worldsnas.starplayer.model.Music
 
 class MusicsListAdapter :
     ListAdapter<Music, MusicListItemViewHolder>(DiffCallback) {
+    var onItemClick: ((Music) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicListItemViewHolder {
         val musicViewBinding: ItemMusicBinding =
             ItemMusicBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MusicListItemViewHolder(musicViewBinding)
+        return MusicListItemViewHolder(musicViewBinding,onItemClick)
     }
 
     override fun onBindViewHolder(holder: MusicListItemViewHolder, position: Int) {
@@ -26,7 +24,7 @@ class MusicsListAdapter :
     }
 }
 
-class MusicListItemViewHolder(private val mBinding: ItemMusicBinding) :
+class MusicListItemViewHolder(private val mBinding: ItemMusicBinding,val onItemClick: ((Music) -> Unit)?) :
     BaseViewHolder<Music>(mBinding.root) {
 
     override fun onBind(obj: Music) {
@@ -36,21 +34,11 @@ class MusicListItemViewHolder(private val mBinding: ItemMusicBinding) :
         sendToPlay(obj)
     }
 
-    private fun sendToPlay(obj: Music) {
+   private fun sendToPlay(obj: Music) {
         mBinding.root.setOnClickListener {
-            val bundle = bundleOf(
-                "id" to obj.id,
-                "title" to obj.title,
-                "artist" to obj.artist,
-                "album" to obj.album,
-                "genre" to obj.genre,
-                "address" to obj.address
-            )
-            itemView.findNavController()
-                .navigate(R.id.action_musicsListFragment_to_playerFragment, bundle)
+            onItemClick?.invoke(obj)
         }
     }
-
 }
 
 object DiffCallback : DiffUtil.ItemCallback<Music>() {
