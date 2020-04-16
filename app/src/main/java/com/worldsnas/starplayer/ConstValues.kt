8b8e5bd3b31a41e1248fe711b8b3a064c73700b1
neptu39701 <neptu39701@gmail.com>
2020-04-17 00:59:31 +0430

    Add permission request code to ConstValues

--- app/src/main/java/com/worldsnas/starplayer/ConstValues.kt
+++ app/src/main/java/com/worldsnas/starplayer/ConstValues.kt
@@ -2,6 +2,8 @@ package com.worldsnas.starplayer
 
 object ConstValues {
 
-    const val musicInfoBundleKey = "musicInfo"
-    const val preAddress = "content://media/"
+    const val BUNDLE_KEY_MUSIC_INFO = "musicInfo"
+    const val PRE_ADDRESS_VOLUME = "content://media/"
+    const val READ_EXTERNAL_STORAGE_REQUEST_CODE = 100
+
 }
