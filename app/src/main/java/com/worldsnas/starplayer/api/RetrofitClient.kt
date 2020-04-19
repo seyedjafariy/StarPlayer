9f1388801e0752f09090fc9e4f4f21ebdde0007d
neptu39701 <neptu39701@gmail.com>
2020-04-19 15:25:55 +0430

    Add BaseUrl from ConstValues

--- app/src/main/java/com/worldsnas/starplayer/api/RetrofitClient.kt
+++ app/src/main/java/com/worldsnas/starplayer/api/RetrofitClient.kt
@@ -1,13 +1,13 @@
 package com.worldsnas.starplayer.api
 
+import com.worldsnas.starplayer.ConstValues
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitClient {
-    var BASE_URL: String = "http://starplayerservice.herokuapp.com/"
     private fun retrofit(): Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
+        .baseUrl(ConstValues.BASE_URL)
         .addConverterFactory(MoshiConverterFactory.create())
         .client(OkHttpClient().newBuilder().build())
         .build()
