aafc8a66092679a7d3161119afd5e732e815fa1a
neptu39701 <neptu39701@gmail.com>
2020-04-23 01:23:11 +0430

    Add localMusicProvider and api to repository

--- app/src/main/java/com/worldsnas/starplayer/model/RepositoryImpl.kt
+++ app/src/main/java/com/worldsnas/starplayer/model/RepositoryImpl.kt
@@ -1,14 +1,30 @@
 package com.worldsnas.starplayer.model
 
+import android.util.Log
 import com.worldsnas.starplayer.api.MusicResponse
 import com.worldsnas.starplayer.api.WebServiceApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class RepositoryImpl @Inject constructor(private val webServiceApi: WebServiceApi) : Repository {
-    override suspend fun getDataFromApi(): List<MusicResponse> {
+class RepositoryImpl @Inject constructor(
+    private val localMusicProvider: LocalMusicProvider,
+    private val webServiceApi: WebServiceApi
+) : Repository {
 
-        return webServiceApi.getMusics()
+    override suspend fun getApiData(): List<MusicResponse> {
+
+        return withContext(Dispatchers.IO) {
+            val musicList = webServiceApi.getMusics()
+            Log.d("myTag", musicList.toString())
+            return@withContext musicList
+        }
     }
+
+    override suspend fun getLocalData(): List<LocalMusic> =
+        withContext(Dispatchers.IO) {
+            return@withContext localMusicProvider.getAllMusic()
+        }
 
 
 }
