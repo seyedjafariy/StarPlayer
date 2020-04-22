ec05090d2c403680b13c43a1799563616656a479
neptu39701 <neptu39701@gmail.com>
2020-04-23 01:23:46 +0430

    Add endPointModule to App Component

--- app/src/main/java/com/worldsnas/starplayer/di/components/AppComponent.kt
+++ app/src/main/java/com/worldsnas/starplayer/di/components/AppComponent.kt
@@ -1,16 +1,19 @@
 package com.worldsnas.starplayer.di.components
 
 import android.app.Application
+import com.worldsnas.starplayer.api.WebServiceApi
+import com.worldsnas.starplayer.di.modules.EndPointModule
 import com.worldsnas.starplayer.di.modules.NetworkModule
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
-@Component(modules = [NetworkModule::class])
+@Component(modules = [NetworkModule::class, EndPointModule::class])
 @Singleton
 interface AppComponent {
 
     fun provideApplication(): Application
+    fun provideWebserviceApi(): WebServiceApi
 
     @Component.Builder
     interface Builder {
