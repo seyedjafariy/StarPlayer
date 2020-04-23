26f71723da7c088cfeffbad480d06c055ea7428b
neptu39701 <neptu39701@gmail.com>
2020-04-24 00:01:42 +0430

    Add retrofit Response to WebserviceApi

--- app/src/main/java/com/worldsnas/starplayer/api/WebServiceApi.kt
+++ app/src/main/java/com/worldsnas/starplayer/api/WebServiceApi.kt
@@ -1,9 +1,10 @@
 package com.worldsnas.starplayer.api
 
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface WebServiceApi {
 
     @GET("/api/request")
-   suspend fun getMusics(): List<MusicResponse>
+    suspend fun getMusics(): Response<List<MusicResponse>>
 }
