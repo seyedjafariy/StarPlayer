6cb684457bba0944310306fec5a993947cf99877
neptu39701 <neptu39701@gmail.com>
2020-04-30 21:26:20 +0430

    Add MusicRepository in MusicProvider Module

--- app/src/main/java/com/worldsnas/starplayer/di/modules/MusicProviderModule.kt
+++ app/src/main/java/com/worldsnas/starplayer/di/modules/MusicProviderModule.kt
@@ -1,8 +1,11 @@
 package com.worldsnas.starplayer.di.modules
 
 import android.app.Application
+import com.worldsnas.starplayer.api.WebServiceApi
 import com.worldsnas.starplayer.model.LocalMusicProvider
 import com.worldsnas.starplayer.model.LocalMusicProviderImpl
+import com.worldsnas.starplayer.model.MusicRepository
+import com.worldsnas.starplayer.model.MusicRepositoryImpl
 import dagger.Module
 import dagger.Provides
 
@@ -15,4 +18,12 @@ object MusicProviderModule {
         return LocalMusicProviderImpl(application.contentResolver)
     }
 
+    @JvmStatic
+    @Provides
+    fun provideMusicRepository(
+        webServiceApi: WebServiceApi,
+        localMusicProvider: LocalMusicProvider
+    ): MusicRepository {
+        return MusicRepositoryImpl(localMusicProvider, webServiceApi)
+    }
 }
