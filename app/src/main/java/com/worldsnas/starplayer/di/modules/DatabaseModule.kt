5be99ef46f96a74f49fbde9d76240acc4efa385b
neptu39701 <neptu39701@gmail.com>
2020-05-11 21:54:56 +0430

    Add database to DI graph

---
+++ app/src/main/java/com/worldsnas/starplayer/di/modules/DatabaseModule.kt
@@ -0,0 +1,16 @@
+package com.worldsnas.starplayer.di.modules
+
+import android.app.Application
+import androidx.room.Room
+import com.worldsnas.starplayer.model.persistent.AppDataBase
+import dagger.Module
+import dagger.Provides
+
+@Module
+object DatabaseModule {
+
+    @Provides
+    @JvmStatic
+    fun provideRoom(application: Application) =
+        Room.databaseBuilder(application, AppDataBase::class.java, "starDb").build()
+}
