3f00a11a67da72e91fa3849947e78650cc4ee0b9
neptu39701 <neptu39701@gmail.com>
2020-05-12 14:33:21 +0430

    Add table name to entity

--- app/src/main/java/com/worldsnas/starplayer/model/persistent/FavoriteMusic.kt
+++ app/src/main/java/com/worldsnas/starplayer/model/persistent/FavoriteMusic.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "favorites")
 data class FavoriteMusic(
     @PrimaryKey val id: Int,
     @ColumnInfo(name = "title") val title: String,
