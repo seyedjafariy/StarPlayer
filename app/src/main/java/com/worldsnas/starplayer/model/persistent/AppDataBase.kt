67d052892be09c51602d3d231321e2771a4f600c
neptu39701 <neptu39701@gmail.com>
2020-05-12 16:08:38 +0430

    Add desired Annotation

--- app/src/main/java/com/worldsnas/starplayer/model/persistent/AppDataBase.kt
+++ app/src/main/java/com/worldsnas/starplayer/model/persistent/AppDataBase.kt
@@ -1,7 +1,9 @@
 package com.worldsnas.starplayer.model.persistent
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
 
+@Database(entities = [FavoriteMusic::class], version = 1)
 abstract class AppDataBase : RoomDatabase() {
     abstract fun favoriteMusicDao(): FavoriteMusicDao
 }
