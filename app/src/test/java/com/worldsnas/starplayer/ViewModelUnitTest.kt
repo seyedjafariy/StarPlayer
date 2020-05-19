4ac0f82df348ead73353a1cc7da46c9aa9f22101
hamed <hseify69@gmail.com>
2020-05-20 00:46:15 +0430

    start add test for LocalMusicProviderImpl

--- app/src/test/java/com/worldsnas/starplayer/ViewModelUnitTest.kt
+++ app/src/test/java/com/worldsnas/starplayer/ViewModelUnitTest.kt
@@ -1,23 +1,18 @@
 package com.worldsnas.starplayer
 
+import android.content.ContentResolver
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Observer
-import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
-import com.worldsnas.starplayer.model.MusicRepoModel
+import com.worldsnas.starplayer.model.LocalMusicProviderImpl
 import com.worldsnas.starplayer.model.MusicRepository
 import com.worldsnas.starplayer.view.musics_list.MusicListViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.newSingleThreadContext
-import kotlinx.coroutines.test.setMain
-import org.junit.Test
+import kotlinx.coroutines.test.*
+import org.junit.*
 
-import org.junit.Before
-import org.junit.Rule
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import org.mockito.ArgumentCaptor
-import org.mockito.Mockito.verify
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -26,34 +21,40 @@ import org.mockito.Mockito.verify
  */
 
 @RunWith(JUnit4::class)
-class ExampleUnitTest {
+class ViewModelUnitTest {
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
-
     private val mainThreadSurrogate = newSingleThreadContext("Mocked UI thread")
-
-    lateinit var musicListViewModel: MusicListViewModel
-    lateinit var musicListObserver: Observer<List<MusicRepoModel>>
 
     @Before
     fun setUp() {
         Dispatchers.setMain(mainThreadSurrogate)
 
-        musicListObserver = mock()
     }
 
     @Test
     fun testMusicListViewModel() {
         var musicRepository = mock<MusicRepository>()
-        musicListViewModel = MusicListViewModel(musicRepository)
+        var musicListViewModel = MusicListViewModel(musicRepository)
+        musicListViewModel.postMusic().observeForever {}
 
-        val listClass = ArrayList::class.java as Class<List<MusicRepoModel>>
-        val argumentCaptor = ArgumentCaptor.forClass(listClass)
+        Assert.assertNull( musicListViewModel.postMusic().value)
+    }
 
-        musicListViewModel.postMusic().observeForever(musicListObserver)
+    private val testCoroutineDispatcher = TestCoroutineDispatcher()
+    private val testCoroutineScope = TestCoroutineScope(testCoroutineDispatcher)
 
-        verify(musicListObserver).onChanged(argumentCaptor.capture())
+    @Test
+    fun testLocalMusicProviderImpl() =testCoroutineScope.runBlockingTest{
+        var contentResolver = mock<ContentResolver>()
+        var localMusicProviderImpl = LocalMusicProviderImpl(contentResolver)
+        var localMusicList=localMusicProviderImpl.getAllMusic()
+        Assert.assertNotNull(localMusicList)
+    }
 
+    @After
+    fun destroy() {
+        Dispatchers.resetMain()
     }
 }
