cdeb96e1cb01739ed2b867ef92554420ca4a601f
hamed <hseify69@gmail.com>
2020-05-21 15:52:50 +0430

    start add test for LocalMusicProviderImpl

--- app/src/test/java/com/worldsnas/starplayer/ViewModelUnitTest.kt
+++ app/src/test/java/com/worldsnas/starplayer/ViewModelUnitTest.kt
@@ -14,7 +14,7 @@ import org.junit.*
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
-/**
+/*
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
@@ -30,7 +30,6 @@ class ViewModelUnitTest {
     @Before
     fun setUp() {
         Dispatchers.setMain(mainThreadSurrogate)
-
     }
 
     @Test
