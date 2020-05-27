453218989b902355bb83c442803561eefccdfa3e
neptu39701 <neptu39701@gmail.com>
2020-05-27 18:13:50 +0430

    Removed unnecessary functions

--- app/src/main/java/com/worldsnas/starplayer/model/MusicRepository.kt
+++ app/src/main/java/com/worldsnas/starplayer/model/MusicRepository.kt
@@ -1,12 +1,10 @@
 package com.worldsnas.starplayer.model
 
-import com.worldsnas.starplayer.model.persistent.FavoriteMusic
 
 interface MusicRepository {
 
     suspend fun getApiData(page: Int, count: Int): List<MusicRepoModel>
     suspend fun getLocalData(): List<MusicRepoModel>
-    suspend fun saveToDatabase()
-    suspend fun favoriteItemHandler(favoriteMusic: FavoriteMusic)
+    suspend fun favoritesHandler(musicRepoModel: MusicRepoModel)
 
 }
