435b7c06b19d010d4e254233e698851baf4198c1
neptu39701 <neptu39701@gmail.com>
2020-07-22 16:18:57 +0430

    Refactor to use Flow and LiveData

--- app/src/main/java/com/worldsnas/starplayer/view/FavoritesViewModel.kt
+++ app/src/main/java/com/worldsnas/starplayer/view/FavoritesViewModel.kt
@@ -1,25 +1,28 @@
 package com.worldsnas.starplayer.view
 
+
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.worldsnas.starplayer.model.MusicRepoModel
 import com.worldsnas.starplayer.model.MusicRepository
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class FavoritesViewModel @Inject constructor(private val musicRepository: MusicRepository) : ViewModel() {
-    private val postMusicList = MutableLiveData<List<MusicRepoModel>>()
+class FavoritesViewModel @Inject constructor(private val musicRepository: MusicRepository) :
+    ViewModel() {
+    private lateinit var postMusicList: LiveData<List<MusicRepoModel>>
 
     init {
         getFavoriteMusic()
     }
+
     private fun getFavoriteMusic() =
         viewModelScope.launch {
-            val musicsList = musicRepository.getFavoriteData()
-            postMusicList.postValue(musicsList)
+            postMusicList = musicRepository.getFavoriteData().asLiveData()
         }
+
     fun postMusic(): LiveData<List<MusicRepoModel>> =
         postMusicList
 
